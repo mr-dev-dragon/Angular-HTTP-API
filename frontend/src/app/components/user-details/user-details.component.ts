f2e1e6ee53e5886ed44abd211803ac8a1c14c919
Ahmed Aoulad Amar <71600486+Ahmed-dev-dragon@users.noreply.github.com>
2022-07-20 12:05:43 +0100

    Fetching User Detail

--- frontend/src/app/components/user-details/user-details.component.ts
+++ frontend/src/app/components/user-details/user-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { UserService } from '../service/user.service';
 
 @Component({
   selector: 'app-user-details',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-details.component.scss']
 })
 export class UserDetailsComponent implements OnInit {
-
-  constructor() { }
+data2: any;
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private userService: UserService
+  )
+  { }
 
   ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe((params: ParamMap) =>
+    {  console.log(` Id : ${params.get('uuid')!}`);
+      this.userService.getUser(params.get('uuid')!).subscribe((res:any )=>
+      {
+        console.log(res);
+      return this.data2 = res;
+      }
+      )
+    }
+    )
   }
 
 }
